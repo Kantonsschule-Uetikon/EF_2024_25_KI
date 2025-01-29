9ebf650b40f7bc513bcd00417958779d1729d8c6
Professorkuchen <aliba2605@gmail.com>
2025-01-29 15:35:43 +0100

    Update cr-guessr.py

--- AlexBachmann/cr-guessr.py
+++ AlexBachmann/cr-guessr.py
@@ -1,1 +1,9 @@
 import csv
+import tensorflow as tf
+import pandas as pd
+
+cr_trainer = pd.read_csv("dnd_monsters.csv", names=["name", "url", "cr", "type", "size","ac", "hp", "speed", "align", "legendary", "source", "str", "dex", "con", "int", "cha"])
+cr_trainer.head()
+
+cr_features = cr_trainer.copy()
+cr_labels = cr_features.pop("cr")
