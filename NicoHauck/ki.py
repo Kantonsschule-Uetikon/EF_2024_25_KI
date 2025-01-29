1d0750b3942a45aa552fc70b05e02d7690d3d931
DonScarmo <nico.hauck21@gmail.com>
2025-01-29 17:54:42 +0100

    Parameter (weights und bias) initalisieren hinzugefügt

--- NicoHauck/ki.py
+++ NicoHauck/ki.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+#Pre Processing
 marriage_type_map = {"Love": -1, "Arranged": 1}
 urban_rural_map = {"Rural": -1, "Urban": 1}
 family_involvement_map = {"Low": 1, "Moderate": 2, "High": 3}
@@ -7,7 +8,7 @@ divorce_status_map = {"No": -1, "Yes": 1}
 
 daten_unbearbeitet = np.genfromtxt("marriage_divorce_india_with_id.csv", delimiter=",", dtype=str, skip_header=1)
 
-daten = np.array([
+daten  = np.array([
     [
         float(row[1]),  # Marriage Duration
         float(row[2]),  # Age at Marriage
@@ -16,7 +17,23 @@ daten = np.array([
         urban_rural_map[row[7]],  # Urban/Rural
         family_involvement_map[row[8]],  # Family Involvement
         float(row[9]),  # Children
-        divorce_status_map[row[10]]  # Divorce Status
+        str(row[10])  # Divorce Status
     ]
-    for row in daten_unbearbeitet
-], dtype=float)
+    for row in daten_unbearbeitet])
+
+features_ev = daten[:, :-1].astype(float)[0:200] #_ev für das spätere testen
+features_train = daten[:, :-1].astype(float)[200:1200]
+
+labels_ev = daten[:, -1][0:200]
+labels_train = daten[:, -1][200:1200]
+
+def init_parameter():
+    w1 = np.random.rand(10, 7) - 0.5
+    b1 = np.random.rand(10,) - 0.5
+
+    w2 = np.random.rand(2, 10) - 0.5
+    b2 = np.random.rand(2,) - 0.5
+    
+    return w1, b1, w2, b2
+
+
