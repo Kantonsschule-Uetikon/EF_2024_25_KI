5cb00bb7d448869673dd8c815f3ebb4deb7e53e2
DonScarmo <nico.hauck21@gmail.com>
2025-01-29 17:11:27 +0100

    Pre processing gemacht, Matrix mit floats

--- NicoHauck/daten_aufrauemen.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-daten = np.genfromtxt("marriage_divorce_india_with_id.csv", delimiter=",", dtype=str, skip_header=1)
-
-saubere_daten = daten[:, [1, 2, 3, 5, 7, 8, 9, 10]]
-
-print(saubere_daten)
---
+++ NicoHauck/ki.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+marriage_type_map = {"Love": -1, "Arranged": 1}
+urban_rural_map = {"Rural": -1, "Urban": 1}
+family_involvement_map = {"Low": 1, "Moderate": 2, "High": 3}
+divorce_status_map = {"No": -1, "Yes": 1}
+
+daten_unbearbeitet = np.genfromtxt("marriage_divorce_india_with_id.csv", delimiter=",", dtype=str, skip_header=1)
+
+daten = np.array([
+    [
+        float(row[1]),  # Marriage Duration
+        float(row[2]),  # Age at Marriage
+        marriage_type_map[row[3]],  # Marriage Type
+        float(row[5]),  # Income Level
+        urban_rural_map[row[7]],  # Urban/Rural
+        family_involvement_map[row[8]],  # Family Involvement
+        float(row[9]),  # Children
+        divorce_status_map[row[10]]  # Divorce Status
+    ]
+    for row in daten_unbearbeitet
+], dtype=float)
