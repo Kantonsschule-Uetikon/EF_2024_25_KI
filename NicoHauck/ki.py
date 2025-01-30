3cb2f4ccb75b8cf0c4c077b24de32e185f858697
DonScarmo <nico.hauck21@gmail.com>
2025-01-30 17:47:01 +0100

    kaputt

--- NicoHauck/ki.py
+++ NicoHauck/ki.py
@@ -13,7 +13,7 @@ daten  = np.array([
         float(row[1]),  # Marriage Duration
         float(row[2]),  # Age at Marriage
         marriage_type_map[row[3]],  # Marriage Type
-        float(row[5]),  # Income Level
+        float(row[5]),  # Income
         urban_rural_map[row[7]],  # Urban/Rural
         family_involvement_map[row[8]],  # Family Involvement
         float(row[9]),  # Children
@@ -45,10 +45,16 @@ def ReLU(z):
 def ReLU_abl(z):
     return (z > 0).astype(float)
 
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_abl(x):
+    sig = sigmoid(x)
+    return sig * (1 - sig)
+
 def softmax(z):                     #softmax berechnet wahrscheinlichkeiten
     exp_z = np.exp(z - np.max(z))
     return exp_z / np.sum(exp_z)
-
 
 
 def label_zu_vektor(label):
@@ -60,10 +66,10 @@ def label_zu_vektor(label):
 
 def vorwärts(w1, b1, w2, b2, w3, b3, X):
     z1 = w1.dot(X) + b1
-    a1 = ReLU(z1)
+    a1 = sigmoid(z1)
 
     z2 = w2.dot(a1) + b2
-    a2 = ReLU(z2)
+    a2 = sigmoid(z2)
 
     z3 = w3.dot(a2) + b3
     a3 = softmax(z3)
@@ -80,12 +86,12 @@ def rückwärts(z1, a1, z2, a2, z3, a3, w3, w2, X, Y):
     db3 = 1/2 * dz3
     
     # hidden 2
-    dz2 = w3.T.dot(dz3) * ReLU_abl(z2)
+    dz2 = w3.T.dot(dz3) * sigmoid_abl(z2)
     dw2 = 1/2 * np.outer(dz2, a1)
     db2 = 1/2 * dz2
     
     # hidden 1
-    dz1 = w2.T.dot(dz2) * ReLU_abl(z1)
+    dz1 = w2.T.dot(dz2) * sigmoid_abl(z1)
     dw1 = 1/2 * np.outer(dz1, X)
     db1 = 1/2 * dz1
     
@@ -109,8 +115,8 @@ def trainieren(X, Y, iterationen, learning_rate):
         w1, b1, w2, b2, w3, b3 = update_parameter(w1, b1, w2, b2, w3, b3, dw1, db1, dw2, db2, dw3, db3, learning_rate)
     return w1, b1, w2, b2, w3, b3
     
-w1, b1, w2, b2, w3, b3 = trainieren(features_train, labels_train, 10, 0.1)
+w1, b1, w2, b2, w3, b3 = trainieren(features_train, labels_train, 5, 0.1)
 
 
-print(w1, b1, w2, b2, w3, b3)
+
 
