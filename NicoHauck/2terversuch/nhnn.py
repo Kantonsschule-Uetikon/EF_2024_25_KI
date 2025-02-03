d8c98d3900f076cc4411f607a8dffe5e15121a36
DonScarmo <nico.hauck21@gmail.com>
2025-02-03 22:03:40 +0100

    neuer versuch mit oop

---
+++ NicoHauck/2terversuch/nhnn.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+class nhnn():
+    def __init__(self, layers):
+        self.layers = layers
+    
+    class Layer():
+        def __init__(self):
+            pass
+    
+    class Dense(Layer):
+        def __init__(self):
+            pass
+
+    class Acitvation(Layer):
+        def __init__(self):
+            pass
+    
+    def train(self):
+        pass
+
+    def evaluate(self):
+        pass
