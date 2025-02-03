b466d5aed6f047f8fad42382a2cf622e34652d80
DonScarmo <nico.hauck21@gmail.com>
2025-02-03 22:11:15 +0100

    Funktionen hinzugefügt

--- NicoHauck/2terversuch/nhnn.py
+++ NicoHauck/2terversuch/nhnn.py
@@ -9,13 +9,39 @@ class nhnn():
             pass
     
     class Dense(Layer):
-        def __init__(self):
+        def __init__(self, input_neurons, output_neurons):
+            self.input_neurons = input_neurons
+            self.output_neurons = output_neurons
+            self.weights = np.randn(output_neurons, input_neurons)
+
+        def vorwaerts(self):
+            pass
+        
+        def rueckwaerts(self):
             pass
 
-    class Acitvation(Layer):
-        def __init__(self):
+    class Activation(Layer):
+        def __init__(self, neurons):
             pass
-    
+
+    class Sigmoid(Activation):
+        def __init__(self, neurons):
+            pass
+
+        def vorwaerts(self):
+            pass
+        def rueckwaerts(self):
+            pass
+        
+    class Softmax(Activation):
+        def __init__(self, neurons):
+            pass
+
+        def vorwaerts(self):
+            pass
+        def rueckwaerts(self):
+            pass
+
     def train(self):
         pass
 
