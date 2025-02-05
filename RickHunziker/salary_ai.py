7be717e44fb3c22c4e066f64bdb9e879675a8f69
knojel <109758896+knojel@users.noreply.github.com>
2025-02-05 15:38:45 +0100

    create categories for the salaries

--- RickHunziker/salary_ai.py
+++ RickHunziker/salary_ai.py
@@ -12,18 +12,30 @@ with open("Salary_Dataset.csv", "r") as csvfile:
 
 data = np.array(data) # Easier to work with np arrays
 
-print(data[:5])
-
 # Convert gender ('Male' and 'Female' into '1' and '0')
 gender = data[:, 1] # The second column of every row
 encoder = LabelEncoder()
 gender_encoded = encoder.fit_transform(gender)
 data[:, 1] = gender_encoded
 
-print(data[:5])
-
 # Convert Age, Gender, Education Level, Years of Experience, Senior to float;
 # Create matrix with features x datasets
 X = data[:, [0, 1, 2, 4, 8]].astype(float)
 
-print(X)
+# Convert Salary to float;
+y = data[:, 5].astype(float)
+
+print(y)
+
+# Group Salary into 4 categories -> Labels
+for i in range(len(y)):
+    if y[i] < 70000:
+        y[i] = 0
+    elif y[i] < 115000:
+        y[i] = 1
+    elif y[i] < 160000:
+        y[i] = 2
+    else:
+       y[i] = 3
+
+print(y)
