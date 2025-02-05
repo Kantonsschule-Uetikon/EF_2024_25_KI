bd4bbbdfcf8fcf835053e3441dce2e74227f9633
knojel <109758896+knojel@users.noreply.github.com>
2025-02-05 18:49:21 +0100

    define, compile, train and evaluate model

--- RickHunziker/salary_ai.py
+++ RickHunziker/salary_ai.py
@@ -2,6 +2,9 @@ import csv
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Input, Dense, Dropout
 
 # Load the Salary dataset
 data = [] # used as a matrix
@@ -26,8 +29,6 @@ X = data[:, [0, 1, 2, 4, 8]].astype(float)
 # Convert Salary to float;
 y = data[:, 5].astype(float)
 
-print(y)
-
 # Group Salary into 4 categories -> Labels
 for i in range(len(y)):
     if y[i] < 70000:
@@ -39,11 +40,30 @@ for i in range(len(y)):
     else:
        y[i] = 3
 
-print(y)
-
 # One-hot encode the target variable
 # One-hot encoding -> convert each category into a binary vector
 encoder_one_hot = OneHotEncoder(sparse_output=False)
-y_one_hot = encoder_one_hot.fit_transform(y.reshape(-1, 1))
+y_one_hot = encoder_one_hot.fit_transform(y.reshape(-1, 1)) 
 
-print(y_one_hot)
+# Split data into training and testing sets
+# 80% of data for training; 20% for testing
+# random_state to ensure categorisation always the same  (reproducibility)
+X_train, X_test, y_train, y_test = train_test_split(X, y_one_hot, test_size=0.2, random_state=42)
+
+# Define the model
+model = Sequential()
+model.add(Input(shape=(5,))) # 5 features
+model.add(Dense(32, activation='relu')) # relu = rectified linear unit; negative values = 0
+model.add(Dropout(0.2)) # get rid of 20 % to prevent overfitting
+model.add(Dense(16, activation='relu'))
+model.add(Dense(4, activation='softmax')) # 4 salary categories
+
+# Compile model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(X_train, y_train, epochs=50, batch_size=32, validation_split=0.2)
+
+# Evaluate the model
+loss, accuracy = model.evaluate(X_test, y_test)
+print(f'Loss: {loss}, Accuracy: {accuracy}')
