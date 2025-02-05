53807bbbe1f508115377a6daa927800b3e8aca0b
Simon <kwizktz@hotmail.com>
2025-02-05 16:15:55 +0100

    Simon_Final

--- SimonTobler/SN_Analyse.py
+++ SimonTobler/SN_Analyse.py
@@ -29,9 +29,9 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 model = Sequential()
 model.add(Input((2,)))
-model.add(Dense(50, activation='relu'))
+model.add(Dense(18, activation='relu'))
 #model.add(Dense(100, activation='relu'))
-model.add(Dense(50, activation='relu'))
+model.add(Dense(18, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
 model.compile(optimizer='adam', 
