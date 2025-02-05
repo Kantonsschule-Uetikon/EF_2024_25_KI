7a74de2c2d90221d552f3a9f6bd21949a2553b7c
knojel <109758896+knojel@users.noreply.github.com>
2025-02-05 15:04:29 +0100

    convert gender into numbers

--- RickHunziker/salary_ai.py
+++ RickHunziker/salary_ai.py
@@ -1,12 +1,23 @@
-# Test CSV-Datei einlesen
 import csv
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
 
 # Load the Salary dataset
-data = []
+data = [] # used as a matrix
 with open("Salary_Dataset.csv", "r") as csvfile:
     reader = csv.reader(csvfile)
     next(reader) # skip header
     for row in reader:
         data.append(row)
 
-print(data[:50])
+data = np.array(data) # Easier to work with np arrays
+
+print(data[:5])
+
+# Convert gender ('Male' and 'Female' into numbers)
+gender = data[:, 1] # The second column of every row
+encoder = LabelEncoder()
+gender_encoded = encoder.fit_transform(gender)
+data[:, 1] = gender_encoded
+
+print(data[:5])
